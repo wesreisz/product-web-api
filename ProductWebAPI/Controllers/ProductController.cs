38c57f689ee0e1f531c76be551e7ff817b7ca25b
Wesley Reisz <wes@wesleyreisz.com>
2016-03-29 18:52:50 -0500

    Added Files

---
+++ ProductWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ProductWebAPI.Models;
+using ProductWebAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductWebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public IEnumerable<Product> GetAllProducts() {
+            IProductService service = new ProductService();
+            return service.getAllProducts();
+        }
+
+        public IHttpActionResult GetProduct(int id) {
+            IProductService service = new ProductService();
+            var product = service.getProductById(id);
+
+            if (product == null) {
+                return NotFound();
+            }
+            else {
+                return Ok(product);
+            }
+        }
+    }
+}
---
+++ ProductWebAPI/Service/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProductWebAPI.Models;
+using ProductWebAPI.DAL;
+
+namespace ProductWebAPI.Service
+{
+    public class ProductService : IProductService
+    {
+        public IEnumerable<Product> getAllProducts()
+        {
+            List<Product> products = new List<Product>();
+            using (var repo = new SampleModel()) {
+                foreach (var petsDomainModel in repo.Pets) {
+                    Product p = new Product();
+                    p.Id = (int)petsDomainModel.PetID;
+                    p.Name = petsDomainModel.PetName + " (" + petsDomainModel.PetDescription +")";
+                    p.Price = petsDomainModel.PetPrice;
+                    p.Category = petsDomainModel.AnimalType.AnimalName;
+                    products.Add(p);
+                }
+            }
+
+            return products;
+        }
+        
+        public Product getProductById(int id)
+        {
+            Product result = null;
+            IEnumerable < Product > products = getAllProducts();
+            foreach (Product p in products) {
+                if (p.Id == id)
+                {
+                    result = p;
+                }
+            }
+            return result;
+        }
+    }
+}
